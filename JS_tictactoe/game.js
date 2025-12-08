7c10ebc2255eaaf41b2e511a184edaf889c39a15
khe4oyan <vahe_harutyunyan00@mail.ru>
2025-12-08 21:35:01 +0400

    Improved the UI and fixed some bugs

--- JS_tictactoe/game.js
+++ JS_tictactoe/game.js
@@ -1,6 +1,7 @@
 const cells = document.querySelectorAll('.cell');
 const statusText = document.querySelector('#statusText');
 const restartnBtn = document.querySelector('#restartBtn');
+restartnBtn.style.display = 'none';
 
 const board = [
   [0, 1, 2],
@@ -23,7 +24,6 @@ let count = 0;
 function initializeGame() {
   cells.forEach((cell) => cell.addEventListener('click', cellClicked));
   restartnBtn.addEventListener('click', restartGame);
-  statusText.textContent = `${currentPlayer}'s turn`;
   updatePlayerUI();
   runing = true;
 }
@@ -39,7 +39,7 @@ function cellClicked() {
   if (count >= 3) {
     checkBoard();
   }
-  
+
   if (runing) {
     changePlayer();
     updatePlayerUI();
@@ -55,19 +55,17 @@ function updateCell(cell, index) {
 
 function changePlayer() {
   currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-  statusText.textContent = `${currentPlayer}'s turn`;
 }
 
 function updatePlayerUI() {
   const xBox = document.getElementById('x-side');
   const oBox = document.getElementById('o-side');
 
-  xBox.classList.remove('active');
-  oBox.classList.remove('active');
-
   if (currentPlayer === 'X') {
+    oBox.classList.remove('active');
     xBox.classList.add('active');
   } else {
+    xBox.classList.remove('active')
     oBox.classList.add('active');
   }
 }
@@ -79,34 +77,43 @@ function checkBoard() {
   for (let i = 0; i < board.length; ++i) {
     const [a, b, c] = board[i];
 
-    if (columns[a] !== '' && 
-        columns[a] === columns[b] &&
-        columns[b] === columns[c]) {
-       roundWon = true;
-       break;
+    if (
+      columns[a] !== '' &&
+      columns[a] === columns[b] &&
+      columns[b] === columns[c]
+    ) {
+      roundWon = true;
+      break;
     }
   }
 
   if (roundWon) {
-    statusText.textContent = `${currentPlayer} wins!`;
+    statusText.textContent = `${currentPlayer} Won!`;
     runing = false;
   } else if (!columns.includes('')) {
     statusText.textContent = `Draw!`;
     runing = false;
   }
+
+  if (!runing) {
+    restartnBtn.style.display = 'inline-block';
+  }
 }
 
 function restartGame() {
   cells.forEach((cell) => {
+    const xBox = document.getElementById('x-side');
     const oBox = document.getElementById('o-side');
     cell.textContent = '';
     cell.classList.remove('x', 'o');
     oBox.classList.remove('active');
+    xBox.classList.add('active');
   });
+  restartnBtn.style.display = 'none';
+  statusText.textContent = '';
   columns = ['', '', '', '', '', '', '', '', ''];
   currentPlayer = 'X';
   count = 0;
-  statusText.textContent = `${currentPlayer}'s turn!`;
   runing = true;
 }
 
